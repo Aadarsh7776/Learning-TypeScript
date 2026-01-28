1b3588d479db39c2ad9e2994001f2b8109e52369
Aadarsh7776 <aadarshsapkal@gmail.com>
2026-01-28 19:37:35 +0530

    learned about control statements in type script

---
+++ dist/control-statements.js
@@ -0,0 +1,44 @@
+// if-else..
+const age = 17;
+if (age < 18) {
+    console.log("Underage!!");
+}
+else {
+    console.log("Have a great dayy:)");
+}
+// nested if else 
+const grade = 64;
+if (grade < 35) {
+    console.log("Have a great dayy:)");
+}
+else if ((35 <= grade) && (grade <= 75)) {
+    console.log("Average", +grade);
+}
+else {
+    console.log("Excellent");
+}
+// for loop
+let sum1 = 0;
+for (let counter = 0; counter <= 10; counter++) {
+    sum1 = sum1 + counter;
+    console.log(sum1);
+}
+//do while
+var iteration = 0;
+do {
+    console.log("iteration no:", +iteration);
+    iteration++;
+} while (iteration < 10);
+const arr = [10, 20, 30, 40];
+for (const index in arr) { //in for kys or indices and of for data 
+    console.log(index);
+    console.log(arr[index]);
+}
+//while
+var i1 = 0;
+while (i1 <= 10) {
+    if (i1 % 2 === 0) {
+        console.log(`${i1} is even`);
+    }
+    i1++;
+}
---
+++ src/control-statements.ts
@@ -0,0 +1,39 @@
+// if-else..
+const age : number = 17;
+
+if(age < 18){
+    console.log("Underage!!")
+}else{
+    console.log("Have a great dayy:)")
+}
+
+// nested if else 
+const grade:number = 64
+
+if(grade < 35){
+    console.log("Have a great dayy:)")
+}else if((35 <= grade) && (grade <= 75)){
+    console.log("Average",+grade)   
+}else{
+    console.log("Excellent")
+}
+
+// for loop
+let sum1 : number = 0
+for (let counter=0;counter <= 10;counter++){
+    sum1 = sum1 + counter;
+    console.log(sum1)
+}
+
+//do while
+var iteration:number = 0
+do{
+    console.log("iteration no:",+iteration)
+    iteration++;
+}while(iteration < 10)
+
+const arr: Array<number> = [10,20,30,40]
+for (const index in arr){ //in for kys or indices and of for data 
+    console.log(index)
+    console.log(arr[index])
+}
